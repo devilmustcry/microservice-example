07f5035ee6813ac870e3e062179f7bb6125eab08
Arut Thanomwatana <arut.tha@gmail.com>
2018-10-04 18:21:40 +0700

    Microservice example

---
+++ Product-Service/src/main/java/com/example/ProductService/controllers/ProductController.java
@@ -0,0 +1,31 @@
+package com.example.ProductService.controllers;
+import com.example.ProductService.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping(value = "products")
+public class ProductController {
+
+    @LoadBalanced
+    @Bean
+    private RestTemplate restTemplate(){
+      return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/search")
+    public Product getProductsByTypeAndName(@RequestParam(value = "sku") final String sku) {
+      String url = "http://PRICING-SERVICE/products/price?sku=" + sku;
+      return restTemplate.getForObject(url, Product.class);
+    }
+}
+
---
+++ Product-Service/src/main/java/com/example/ProductService/models/Product.java
@@ -0,0 +1,12 @@
+package com.example.ProductService.models;
+
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    private String sku;
+    private String price;
+}
